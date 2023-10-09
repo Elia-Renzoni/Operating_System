e6f225574e9d1e54ea0728b4787a58fb2d2edfad
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-10-09 16:37:01 +0200

    Add files via upload

--- Concurrent_Programmin_Rep/Go_Concurrency/go_routine_ex.go
+++ Concurrent_Programmin_Rep/Go_Concurrency/go_routine_ex.go
@@ -1,1 +1,37 @@
+/*
+*
+*	@author Elia Renzoni
+*	@date 09/10/2023
+*	@brief Go routine first example
+*
+ */
+package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type arr [12]int
+
+func main() {
+	var nums *arr = new(arr)
+	go initArray(nums)
+	go sumArrayElements(nums)
+}
+
+func initArray(nums *arr) {
+	rand.Seed(time.Now().Unix())
+	for i := range nums {
+		nums[i] = rand.Intn(100)
+	}
+}
+
+func sumArrayElements(nums *arr) {
+	var sum int
+	for j := range nums {
+		sum += nums[j]
+	}
+	fmt.Printf("Sum = %d", sum)
+}
